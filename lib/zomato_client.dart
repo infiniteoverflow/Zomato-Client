8530fd412f47939042334d2bc40e5b8789f0cef9
infiniteoverflow <aswingopinathan1871@gmail.com>
2020-12-16 16:10:53 +0530

    Added Initial Files

---
+++ lib/zomato_client.dart
@@ -0,0 +1,10 @@
+library zomato_client;
+
+/// A Dart class to get all the endpoints of the Zomato API.
+class Zomato {
+
+  // The Zomato API Key
+  String key;
+
+  Zomato({this.key});
+}
---
+++ test/zomato_client_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:zomato_client/zomato_client.dart';
+
+void main() {
+  
+}
